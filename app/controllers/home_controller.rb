1e5650e156b5d266bc674019eda338f9e9ff65de
etlds <etlds@hotmail.com>
2014-12-03 12:41:29 -0500

    cell

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
 	def index
-		
+		if params[:Kind] == ""
+		elsif params[:Kind] == "cell"
+			@products = Product.where(Kind: "cell")
+			render "cell"
+		end
 	end
 end
