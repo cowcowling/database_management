33f419a125e82fbaba9951eb28865610d1fcdba4
etlds <etlds@hotmail.com>
2014-12-04 16:32:24 -0500

    thur 2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
 	def index
 		if params[:Kind].nil?
+			if params[:keyword]
+				@products = Product.where(name: params[:keyword])
+				render "kind"
+			end
 		else
 			@products = Product.where(Kind: params[:Kind])
 			render "kind"
