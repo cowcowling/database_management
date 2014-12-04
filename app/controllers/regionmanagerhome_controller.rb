5ee6f972b87b20f7388f755c9fa37276d71f60a9
cowcowling <cow-cowling@hotmail.com>
2014-12-04 17:21:34 -0500

    12/4/2014

--- app/controllers/regionmanagerhome_controller.rb
+++ app/controllers/regionmanagerhome_controller.rb
@@ -4,5 +4,15 @@ class RegionmanagerhomeController < ApplicationController
 		@transactions = Transaction.where(branch_id: branches.map { |b| b.id })
 		@sales_people = SalesPerson.where(branch_id: branches.map { |b| b.id })
 		@store_managers = StoreManager.where(region_id: params[:regionid])
+		@store_reports = []
+		branches.each do |branch|
+			count = Transaction.where(branch_id: branch.id).count
+			total_sales = 0.0
+			Transaction.where(branch_id: branch.id).each do |t|
+				total_sales += Product.find(t.product_id).price * t.quantity
+			end
+			@store_reports << {id: branch.id, address: branch.address, transaction_count: count, total_sales: total_sales}
+		end
+		puts @store_reports.inspect
 	end
 end
--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,4 +1,5 @@
 class Branch < ActiveRecord::Base
   belongs_to :store_manager
   belongs_to :region
+  has_many :sales_people
 end
