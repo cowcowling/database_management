b03d237f36885f18509008ad2c807f387dcd396e
cowcowling <cow-cowling@hotmail.com>
2014-12-04 21:40:59 -0500

    12/4/2014 9:40pm

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 	def index
 		if params[:Kind].nil?
 			if params[:keyword]
-				@products = Product.where(name: params[:keyword])
+				@products = Product.where("name LIKE ?", "%#{params[:keyword]}%")
 				render "kind"
 			end
 		else
