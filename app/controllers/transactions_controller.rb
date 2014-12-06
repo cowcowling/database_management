812d72595fdd2808629d5d912bd595c7eeba7d3e
cowcowling <cow-cowling@hotmail.com>
2014-12-06 00:20:50 -0500

    /12/6/2014

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -50,8 +50,29 @@ class TransactionsController < ApplicationController
   # PATCH/PUT /transactions/1
   # PATCH/PUT /transactions/1.json
   def update
+    origin_quantity = @transaction.quantity
+    if(Product.find(@transaction.product_id).stock_amount < (transaction_params["quantity"].to_i - origin_quantity))
+      @transaction.errors.add(:stock, "Out of Stock")
+      render :edit
+      return
+    end
     respond_to do |format|
       if @transaction.update(transaction_params)
+        updated_quantity = @transaction.quantity
+
+        if(updated_quantity >= origin_quantity)
+           
+          p = Product.find(@transaction.product_id)
+          p.stock_amount -= (updated_quantity - origin_quantity)
+          p.sale_amount += (updated_quantity - origin_quantity)
+          p.save
+        else
+          p = Product.find(@transaction.product_id)
+          p.stock_amount += (updated_quantity - origin_quantity)
+          p.sale_amount -= (updated_quantity - origin_quantity)
+          p.save
+        end        
+
         format.html { redirect_to @transaction, notice: 'Transaction was successfully updated.' }
         format.json { render :show, status: :ok, location: @transaction }
       else
@@ -59,13 +80,20 @@ class TransactionsController < ApplicationController
         format.json { render json: @transaction.errors, status: :unprocessable_entity }
       end
     end
+
   end
 
   # DELETE /transactions/1
   # DELETE /transactions/1.json
   def destroy
+    origin_quantity = @transaction.quantity
     @transaction.destroy
     respond_to do |format|
+      p = Product.find(@transaction.product_id)
+      p.stock_amount += origin_quantity
+      p.sale_amount -= origin_quantity
+      p.save
+
       format.html { redirect_to transactions_url, notice: 'Transaction was successfully destroyed.' }
       format.json { head :no_content }
     end
